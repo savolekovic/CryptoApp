1a9e2bf308e45f8c52ad1352c7761ab018db99ba
Savo Lekovic <savo.lekovic@amplitudo.me>
2024-11-04 10:46:29 +0100

    Creates CoinItem UI

---
+++ app/src/main/java/com/vosaa/cryptoapp/presentation/coin_list/CoinListItem.kt
@@ -0,0 +1,83 @@
+package com.vosaa.cryptoapp.presentation.coin_list
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.vosaa.cryptoapp.domain.Coin
+import com.vosaa.cryptoapp.presentation.models.CoinUi
+import com.vosaa.cryptoapp.presentation.models.toCoinUi
+import com.vosaa.cryptoapp.ui.theme.CryptoAppTheme
+
+@Composable
+fun CoinListItem(
+    coinUi: CoinUi, onClick: () -> Unit, modifier: Modifier = Modifier
+) {
+
+    val contentColor = if (isSystemInDarkTheme()) Color.White
+    else Color.Black
+
+    Row(
+        modifier = modifier
+            .clickable(onClick = onClick)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(coinUi.iconRes),
+            contentDescription = coinUi.name,
+            tint = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.size(85.dp)
+        )
+
+        Column(modifier = Modifier.weight(1f)) {
+            Text(
+                text = coinUi.symbol,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = contentColor
+            )
+        }
+    }
+}
+
+
+@PreviewLightDark
+@Composable
+private fun CoinListItemPreview() {
+    CryptoAppTheme {
+        CoinListItem(
+            coinUi = previewCoin.toCoinUi(),
+            onClick = { /* TODO */ },
+            modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer)
+        )
+    }
+}
+
+internal val previewCoin = Coin(
+    id = "bitcoin",
+    rank = 1,
+    name = "Bitcoin",
+    symbol = "BTC",
+    marketCapUsd = 1241273958896.75,
+    priceUsd = 62828.15,
+    changePercent24Hr = 0.1
+)
