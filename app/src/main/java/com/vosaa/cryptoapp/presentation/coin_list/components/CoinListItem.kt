ae8f3be2d85ad90d62038da9e2415757f9469924
Savo Lekovic <savo.lekovic@amplitudo.me>
2024-11-04 10:50:15 +0100

    Creates components directory

--- app/src/main/java/com/vosaa/cryptoapp/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/vosaa/cryptoapp/presentation/coin_list/components/CoinListItem.kt
@@ -1,4 +1,4 @@
-package com.vosaa.cryptoapp.presentation.coin_list
+package com.vosaa.cryptoapp.presentation.coin_list.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
