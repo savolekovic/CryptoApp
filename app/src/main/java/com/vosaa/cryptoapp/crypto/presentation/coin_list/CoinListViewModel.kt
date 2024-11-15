afba1662446e82b8b56158b7013bd856df0062cf
Savo Lekovic <savo.lekovic@amplitudo.me>
2024-11-15 11:25:52 +0100

    Sets up action events in viewModel

--- app/src/main/java/com/vosaa/cryptoapp/crypto/presentation/coin_list/CoinListViewModel.kt
+++ app/src/main/java/com/vosaa/cryptoapp/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -30,7 +30,11 @@ class CoinListViewModel(
     fun onAction(action: CoinListAction) {
         when (action) {
             is CoinListAction.OnCoinClick -> {
-
+                _state.update {
+                    it.copy(
+                        selectedCoin = action.coinUi
+                    )
+                }
             }
         }
     }
